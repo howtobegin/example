d8e365cfdc81c759b1f843d025b0a93f752b4e70
lhl <hailin.liu@pactera.com>
2018-07-03 16:10:00 +0800

    webapp

---
+++ webapp-example/src/main/java/org/webapp/HelloController.java
@@ -0,0 +1,17 @@
+package org.webapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@RequestMapping()
+	public String hello(@RequestParam(required = false) String message, Model model) {
+		model.addAttribute("message", message);
+		return "hello";
+	}
+}
